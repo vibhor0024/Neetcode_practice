570bcdd292f65c4a6ed3a09d3d3aa0c0a01ae09e
vibhor0024 <143932462+vibhor0024@users.noreply.github.com>
2025-02-21 16:34:07 -0700

    Time: 11 ms (73.14%) | Memory: 18 MB (49.04%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,25 +1,21 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
         
-        sdict = {}
-        tdict = {}
+        sdict = dict()
+        tdict = dict()
 
         for i in range(len(s)):
             sdict[s[i]] = 1 + sdict.get(s[i],0)
             tdict[t[i]] = 1 + tdict.get(t[i],0)
-
+        
         for c in sdict:
             if sdict[c] != tdict.get(c,0):
                 return False
         
         return True
+
 
 
         
