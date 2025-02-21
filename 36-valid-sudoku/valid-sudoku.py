607b8991ce3212b38631d6574de00cecb8125906
vibhor0024 <143932462+vibhor0024@users.noreply.github.com>
2025-02-21 16:37:17 -0700

    Time: 3 ms (83.12%) | Memory: 17.8 MB (44.02%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,27 +1,24 @@
-class Solution(object):
-    def isValidSudoku(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: bool
-        """
-        column = collections.defaultdict(set)
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
         rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
         squares = collections.defaultdict(set)
 
         for r in range(9):
             for c in range(9):
                 if board[r][c] == '.':
                     continue
-                elif board[r][c] in rows[r] or \
-                board[r][c] in column[c] or \
-                board[r][c] in squares[(r//3,c//3)]:
-                    return False
+                
+                if board[r][c] in rows[r] or   \
+                   board[r][c] in cols[c] or   \
+                   board[r][c] in squares[r//3,c//3]:
+                   return False
+                
                 rows[r].add(board[r][c])
-                column[c].add(board[r][c])
-                squares[(r//3,c//3)].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[r//3,c//3].add(board[r][c])
+
         
         return True
-
-
-
-
+                    
+                    
