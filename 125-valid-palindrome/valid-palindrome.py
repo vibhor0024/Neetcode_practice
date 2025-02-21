b4cbfc441413c12e731abd300e95ea74b4760eae
vibhor0024 <143932462+vibhor0024@users.noreply.github.com>
2025-02-21 16:37:59 -0700

    Time: 15 ms (14.53%) | Memory: 17.9 MB (95.20%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,26 +1,30 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        l,r = 0, len(s) - 1
-        while l<r:
-            while l<r and not self.alphanumeric(s[l]):
-                 l += 1
-            while r>l and not self.alphanumeric(s[r]):
-                 r -= 1  
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        l = 0
+        r = len(s)-1
+
+        while (l<r):
+            while l<r and not self.alphan(s[l]):
+                l += 1
+            while l<r and not self.alphan(s[r]):
+                r -= 1
+            
             if s[l].lower() != s[r].lower():
-                 return False
-            l += 1
+                return False
+            
+            l +=1 
             r -= 1
-        return True
-
-    def alphanumeric(self,letter):
-        return (65 <= ord(letter) <= 90) or \
-        (97 <= ord(letter) <= 122) or \
-        (48 <= ord(letter) <= 57) 
-
-
         
+        return True
+            
 
+    def alphan(self, x):
+        if ord(x) >= 97 and ord(x) <= 122 or  \
+           ord(x) >= 65 and ord(x) <= 90 or   \
+           ord(x) >= 48 and ord(x) <= 57:
+           return True
+        
+        else:
+            return False
+        
