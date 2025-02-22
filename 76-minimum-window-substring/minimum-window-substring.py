57d3dc52db2c28235b96cb44b30f0557da3fb51f
vibhor0024 <143932462+vibhor0024@users.noreply.github.com>
2025-02-21 20:48:30 -0700

    Time: 63 ms (81.20%) | Memory: 18.3 MB (35.03%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t == "": return ""
+
+        window = {}
+        Tcount = {}
+
+        indices = [-1.-1]
+        minimum = float("infinity")
+        have = 0
+        
+        res = [-1,-1]
+        l = 0
+
+        for c in t:
+            Tcount[c] = 1 + Tcount.get(c, 0)
+        need = len(Tcount)
+
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+
+            if c in Tcount and window[c] == Tcount[c]:
+                have +=1
+            
+            while have == need:
+                if r-l+1 < minimum:
+                    res = [l,r]
+                    minimum = r-l+1
+                
+                window[s[l]] -= 1
+                if s[l] in Tcount and window[s[l]] < Tcount[s[l]]:
+                    have -= 1
+                
+                l += 1
+        l,r = res
+        return s[l:r+1] if minimum != float("infinity") else ""
