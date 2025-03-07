8e251c6280c9aae1942c7f2f85241e44a198d54f
vibhor0024 <143932462+vibhor0024@users.noreply.github.com>
2025-03-06 20:15:09 -0700

    Time: 0 ms (100.00%) | Memory: 19 MB (47.69%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        #Iterative DFS (stack)
+
+        # stack = [[root,1]]
+        # res = 0
+
+        # while stack:
+        #     node, depth = stack.pop()
+
+        #     if node:
+        #         res = max(res,depth)
+        #         stack.append([node.left,depth+1])
+        #         stack.append([node.right,depth+1])
+        
+        # return res
+
+        #Iterative BFS (deque)
+
+        if not root:
+            return 0
+
+        level = 0
+
+        d = deque([root])
+
+        while d:
+            
+            for i in range(len(d)):
+                node = d.popleft()
+                if node.left:
+                    d.append(node.left)
+                if node.right:
+                    d.append(node.right)
+            
+            level += 1
+        
+        return level
+        
