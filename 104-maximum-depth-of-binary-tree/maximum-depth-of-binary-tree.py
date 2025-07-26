ea4bc8b756a52e6b3954525e9c169189f85761ff
vibhor0024 <143932462+vibhor0024@users.noreply.github.com>
2025-07-25 17:12:21 -0700

    Time: 0 ms (100.00%) | Memory: 19 MB (78.61%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -21,32 +21,32 @@ class Solution:
         
         # return res
 
-        #Iterative BFS (deque)
-
-        # if not root:
-        #     return 0
-
-        # level = 0
-
-        # d = collections.deque([root])
-
-        # while d:
-            
-        #     for i in range(len(d)):
-        #         node = d.popleft()
-        #         if node.left:
-        #             d.append(node.left)
-        #         if node.right:
-        #             d.append(node.right)
-            
-        #     level += 1
-        
-        # return level
-
-        # Recursive DFS
+        # Iterative BFS (deque)
 
         if not root:
             return 0
+
+        level = 0
+
+        d = collections.deque([root])
+
+        while d:
+            
+            for i in range(len(d)):
+                node = d.popleft()
+                if node.left:
+                    d.append(node.left)
+                if node.right:
+                    d.append(node.right)
+            
+            level += 1
         
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        return level
+
+        # Recursive DFS
+
+        # if not root:
+        #     return 0
+        
+        # return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
         
